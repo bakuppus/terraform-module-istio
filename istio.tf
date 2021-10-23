5c12a79c6ce521920a78fc823646ce342c3de8e9
bakuppus <bakuppus@gmail.com>
2021-10-23 21:28:53 +0530

    commit1

---
+++ istio.tf
@@ -0,0 +1,60 @@
+resource "null_resource" "download_istio" {
+  provisioner "local-exec" {
+    command = "wget https://storage.googleapis.com/istio-release/releases/1.11.3/sources.tar.gz"
+  }
+  # Delete on_destroy
+  provisioner "local-exec" {
+    when    = destroy
+    command = "rm -f sources.tar.gz"
+  }
+}
+
+resource "null_resource" "extract_istio" {
+  provisioner "local-exec" {
+    command = "tar -xvzf sources.tar.gz"
+  }
+  depends_on = [
+    null_resource.download_istio
+  ]
+  # Delete on_destroy
+  provisioner "local-exec" {
+    when    = destroy
+    command = "rm -rf sources"
+  }
+}
+
+resource "helm_release" "base" {
+  name             = "${var.name_prefix}-istio-base"
+  repository       = "sources/istio/manifests/charts/"
+  chart            = "base"
+  namespace        = var.namespace
+  wait             = true
+  create_namespace = true
+  lint             = true
+  values           = var.base_values
+
+}
+
+resource "helm_release" "istio-discovery" {
+  name             = "${var.name_prefix}-istio-discovery"
+  repository       = "sources/istio/manifests/charts/istio-control"
+  chart            = "istio-discovery"
+  namespace        = var.namespace
+  wait             = true
+  create_namespace = true
+  lint             = true
+  values           = var.discovery_values
+
+}
+
+resource "helm_release" "istio-ingress" {
+  name             = "${var.name_prefix}-istio-ingress"
+  repository       = "sources/istio/manifests/charts/gateways"
+  chart            = "istio-ingress"
+  namespace        = var.namespace
+  wait             = true
+  create_namespace = true
+  lint             = true
+  values           = var.ingress_values
+
+}
---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "name_prefix" {
+  description = "Prefix to be used when installing."
+  default     = "testing"
+  type        = string
+}
+
+variable "namespace" {
+  description = "The namespace to install into."
+  default     = "istio-system"
+  type        = string
+}
+
+variable "enable_default_control_plane" {
+  description = "Whether to install a default control plane"
+  default     = true
+  type        = bool
+}
+
+variable "base_values" {
+  description = "Istio base Helm values"
+  default     = []
+  type        = list(string)
+}
+
+variable "discovery_values" {
+  description = "Istio discovery Helm values"
+  default     = []
+  type        = list(string)
+}
+
+variable "ingress_values" {
+  description = "Istio ingress Helm values"
+  default     = []
+  type        = list(string)
+}
